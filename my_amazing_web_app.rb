ec40059185e6494b717864ca2f87b4ba284c4223
Dan Garland <dan@dangarland.co.uk>
2013-09-23 23:02:46 +0100

    Got basic track guessing to work

--- my_amazing_web_app.rb
+++ my_amazing_web_app.rb
@@ -1,6 +1,8 @@
 require 'echonest-ruby-api'
 require 'sinatra'
 require 'rack-flash'
+require 'json'
+require 'pry'
 
 enable :sessions
 set :session_secret, 'super secret'
@@ -11,4 +13,20 @@ get '/' do
 end
 
 post '/tracks' do
+  song = Echonest::Song.new('CJ7TD6W3O4RABVSVP')
+  fingerprint = `ENMFP_codegen/codegen.Linux-x86_64 #{params[:track][:tempfile].path} 10 20`
+  code = JSON.parse(fingerprint).first["code"]
+  song_info = song.identify(code)
+
+  if song_info.empty?
+    flash[:notice] = "Er.. you've got me..."
+  else
+    best_guess = song_info.first
+    artist = best_guess[:artist_name]
+    title = best_guess[:title]
+
+    flash[:notice] = "Was your song #{title} by #{artist}?"
+  end
+
+  redirect '/'
 end
