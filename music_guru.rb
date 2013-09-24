f67c1156a750b6e0055c2094eb433886235eaf22
Dan Garland <dan@dangarland.co.uk>
2013-09-24 20:24:01 +0100

    Using EchoWrap as its more featured

--- music_guru.rb
+++ music_guru.rb
@@ -1,4 +1,4 @@
-require 'echonest-ruby-api'
+require 'echowrap'
 require 'sinatra'
 require 'rack-flash'
 require 'json'
@@ -8,7 +8,9 @@ set :session_secret, 'super secret'
 use Rack::Flash
 
 configure do
-  API_KEY = ENV['API_KEY'] || File.read('api.key')
+  Echowrap.configure do |config|
+    config.api_key = ENV['API_KEY'] || File.read('api.key')
+  end
 end
 
 get '/' do
@@ -16,17 +18,14 @@ get '/' do
 end
 
 post '/tracks' do
-  song = Echonest::Song.new(API_KEY)
   fingerprint = `ENMFP_codegen/codegen.Linux-x86_64 #{params[:track][:tempfile].path} 10 20`
   code = JSON.parse(fingerprint).first["code"]
-  song_info = song.identify(code)
+  song = Echowrap.song_identify(:code => code)
 
-  if song_info.empty?
+  if song.nil?
     flash[:notice] = "Er.. you've got me..."
   else
-    best_guess = song_info.first
-    artist, title = best_guess[:artist_name], best_guess[:title]
-    flash[:notice] = "Was your song #{title} by #{artist}?"
+    flash[:notice] = "Was your song #{song.title} by #{song.artist_name}?"
   end
 
   redirect '/'
